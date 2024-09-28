f6256fa94f8d6ffc9099e65c8ffa7585c969d09d
Jahanzaib-butt <jahanzaibbutt745@gmail.com>
2024-09-28 10:31:21 -0700

    Add profile functionality and template

--- inventry_project/settings.py
+++ inventry_project/settings.py
@@ -130,7 +130,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = (BASE_DIR / 'asert')
+
+STATIC_ROOT = (BASE_DIR / 'assets')
+MEDIA_ROOT = (BASE_DIR / 'media')
+
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
