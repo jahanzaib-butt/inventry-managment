cd76afca044144b8700ff8136bbce0654dbd1345
Jahanzaib-butt <jahanzaibbutt745@gmail.com>
2024-09-27 22:15:41 -0700

    make models and paste topnav in every file

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import product
+from .models import Product,Order
 
 class productAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'quantity')  # Corrected spelling of 'category'
     list_filter = ('category',)
 
 # Register your models here.
-admin.site.register(product, productAdmin)
+admin.site.register(Product, productAdmin)
+admin.site.register(Order)
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,15 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 CATAGORY = (
     ('STATIONARY','Stationary'),
     ('ELECTRONIC','Electronic'),
     ('FURNITURE','Furniture'),
 )
-class product(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=200,null=True)
     category = models.CharField(max_length=200,choices=CATAGORY,null=True)  # Corrected spelling
     quantity = models.PositiveIntegerField(max_length=200,null=True)  # Removed max_length
 
     def __str__(self):
         return f'{self.name}-{self.quantity}'
+class Order(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE,null = True)
+    staff =models.ForeignKey(User,on_delete=models.CASCADE,null = True)
+    order_quantity = models.PositiveBigIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product} ordered by "{self.staff.username}"'
