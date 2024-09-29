2891a886f556bcb687d50911381ff68bfd70c307
Jahanzaib-butt <jahanzaibbutt745@gmail.com>
2024-09-29 06:04:40 -0700

    feat: verify email

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.index,name='dashboard-index'),
     path('staff/',views.staff,name='dashboard-staff'),
@@ -9,4 +9,8 @@ urlpatterns = [
     path('order/',views.order,name='dashboard-order'),
     path('product_delete/<int:pk>/',views.product_delete,name='dashboard-product-delete'),
     path('product_update/<int:pk>/',views.product_update,name='dashboard-product-update'),
+    path('password/',auth_views.PasswordResetView.as_view(),name='password_reset'),
+    path('password_reset_done/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('password_reset_complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
 ]
--- inventry_project/settings.py
+++ inventry_project/settings.py
@@ -145,3 +145,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard-index'
 
 LOGIN_URL = 'Login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+
