2e6798795584f81c04d3c8bb415721e50205b177
Jahanzaib-butt <jahanzaibbutt745@gmail.com>
2024-09-29 01:13:57 -0700

    add delete and update functionality in product sectiond

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('staff/',views.staff,name='dashboard-staff'),
     path('product/',views.product,name='dashboard-product'),
     path('order/',views.order,name='dashboard-order'),
+    path('product_delete/<int:pk>/',views.product_delete,name='dashboard-product-delete'),
+    path('product_update/<int:pk>/',views.product_update,name='dashboard-product-update'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Product
+from .forms import ProductForm
 # Create your views here.
 @login_required(login_url='Login')
 def index(request):
@@ -11,7 +13,38 @@ def staff(request):
 
 @login_required(login_url='Login')
 def product(request):
-    return render(request,'Dashboard/product.html')
+    products = Product.objects.all()
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-product')
+    else:
+        form = ProductForm()
+    context = {'products':products, 'form':form}
+    return render(request,'Dashboard/product.html',context)
+
+@login_required(login_url='Login')
+def product_delete(request,pk):
+    product = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('dashboard-product')
+    context = {'product':product}
+    return render(request,'Dashboard/product_delete.html',context)
+
+@login_required(login_url='Login')
+def product_update(request,pk):
+    product = Product.objects.get(id=pk)
+    if request.method == 'POST':
+        form =ProductForm(request.POST,instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard-product')
+    else:
+        form = ProductForm(instance=product)
+    context = {'form':form}
+    return render(request,'Dashboard/product_update.html',context)
 
 @login_required(login_url='Login')
 def order(request):
